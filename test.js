94b881b66768c0b8101a90fa27d7d6859b4c3de3
rocktimsaikia <rocktimthedev@gmail.com>
2026-08-08 11:39:28 +0530

    test: replace ava and xo with the built-in node test runner

--- test.js
+++ test.js
@@ -1,17 +1,14 @@
-const test = require('ava');
-const getMimeType = require('.');
+const assert = require('node:assert/strict');
+const test = require('node:test');
+const mimeTypeCheck = require('.');
 
-test('check invalid files with extensions', t => {
-	t.throws(() => {
-		getMimeType('foo');
-	}, {
-		message: 'Not a valid extension'
-	});
+test('returns every MIME type for an extension', () => {
+	assert.deepEqual(mimeTypeCheck('png'), ['image/png']);
+	assert.deepEqual(mimeTypeCheck('txt'), ['text/plain']);
+	assert.deepEqual(mimeTypeCheck('mid'), ['audio/midi']);
+	assert.deepEqual(mimeTypeCheck('3gpp'), ['audio/3gpp', 'video/3gpp']);
 });
 
-test('basic MIME type check', t => {
-	t.deepEqual(getMimeType('png'), ['image/png']);
-	t.deepEqual(getMimeType('txt'), ['text/plain']);
-	t.deepEqual(getMimeType('mid'), ['audio/midi']);
-	t.deepEqual(getMimeType('3gpp'), ['audio/3gpp', 'video/3gpp']);
+test('throws on an unknown extension', () => {
+	assert.throws(() => mimeTypeCheck('foo'), {message: 'Not a valid extension'});
 });
